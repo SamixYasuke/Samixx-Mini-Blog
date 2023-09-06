37b8ffc91ffaa5662eab3727f5e1c621fedf19a5
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2023-09-06 14:59:40 +0100

    Update CreateBlog.jsx

--- frontend/src/Pages/CreateBlog.js
+++ frontend/src/Pages/CreateBlog.js
@@ -2,63 +2,100 @@ import { useState } from "react";
 import style from "../Assets/Styling/form.module.css";
 import Axios from "axios";
 import { useNavigate } from "react-router-dom";
+import resizeAndConvertToBase64 from "../Dependables/handleBase64Coversion";
 
 const CreateBlog = () => {
+  const [blogImage, setBlogImage] = useState("");
   const [blogTitle, setBlogTitle] = useState("");
   const [blogAuthor, setBlogAuthor] = useState("");
   const [blogContent, setBlogContent] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) {
+      setError("Please select a valid image file");
+      return;
+    }
+  
+    try {
+      const base64String = await resizeAndConvertToBase64(file);
+      setBlogImage(`data:image/jpeg;base64,${base64String}`);
+      setError("");
+    } catch (error) {
+      setError("Error processing the image");
+    }
+  };
+  
+  
   const postBlog = async (e) => {
     e.preventDefault();
 
-    if (blogTitle.trim() === "" || blogAuthor.trim() === "" || blogContent.trim() === "") {
-      alert("Please fill in all fields");
-    } else {
-        try {
-            await Axios.post("http://localhost:5000/blog", {
-                blogAuthor,
-                blogContent,
-                blogTitle
-            });
-            setBlogTitle("");
-            setBlogAuthor("");
-            setBlogContent("");
-            navigate("/");
-        } catch (error) {
-            alert("Couldn't post Blog!")
-        }
+    if (!blogImage || !blogTitle || !blogAuthor || !blogContent) {
+      setError("Please fill in all fields");
+      return;
+    }
+    setLoading(true);
+
+    try {
+      await Axios.post("http://localhost:5000/blog", {
+        blogImage,
+        blogAuthor,
+        blogContent,
+        blogTitle,
+      });
+      setBlogTitle("");
+      setBlogAuthor("");
+      setBlogContent("");
+      setBlogImage("");
+      setLoading(false);
+      navigate("/");
+    } catch (error) {
+      setError("Couldn't post Blog!");
+      setLoading(false);
     }
   };
 
-
   return (
-    <form>
-      <div className={style.inputContainer}>
-        <input
-          value={blogAuthor}
-          onChange={(e) => setBlogAuthor(e.target.value)}
-          placeholder="Enter Your Anime Anonymous name e.g@AnimeLover123"
-        />
-        <input
-          value={blogTitle}
-          onChange={(e) => setBlogTitle(e.target.value)}
-          placeholder="Enter Your Blog Title"
-        />
+    <>
+      <div className={style.imageContainer}>
+        {blogImage && <img className={style.image} src={blogImage} alt={"k"} />}
       </div>
-      <div className={style.textAreaContainer}>
-        <textarea
-          value={blogContent}
-          onChange={(e) => setBlogContent(e.target.value)}
-          placeholder="Enter Your Article"
-          rows="15"
-          cols="30"
-        ></textarea>
-      </div>
-      <div className={style.buttonContainer}>
-        <button onClick={postBlog}>POST BLOG!</button>
-      </div>
-    </form>
+      {error && <div className={style.error}>{error}</div>}
+      <form>
+        <div className={style.inputContainer}>
+          <input
+            value={blogAuthor}
+            onChange={(e) => setBlogAuthor(e.target.value)}
+            placeholder="Enter Your Anime Anonymous name e.g@AnimeLover123"
+          />
+          <input
+            value={blogTitle}
+            onChange={(e) => setBlogTitle(e.target.value)}
+            placeholder="Enter Your Blog Title"
+          />
+          </div>
+          <div className={style.uploadContainer}>
+          <input type="file" accept=".jpeg, .jpg, .png, .gif" onChange={handleImageUpload} />
+          </div>
+          <div className={style.textAreaContainer}>
+          <textarea
+            value={blogContent}
+            onChange={(e) => setBlogContent(e.target.value)}
+            placeholder="Enter Your Article"
+            rows="15"
+            cols="30"
+          ></textarea>
+        </div>
+        <div className={style.buttonContainer}>
+          <button onClick={postBlog} disabled={loading}>
+            {loading ? "Posting..." : "POST BLOG!"}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
