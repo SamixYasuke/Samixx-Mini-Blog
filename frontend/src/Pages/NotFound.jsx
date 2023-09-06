3e1e100a98df96aa21981818220eaa3cf82ab7c6
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2023-09-07 00:24:54 +0100

    Create NotFound.jsx

---
+++ frontend/src/Pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import style from "../Assets/Styling/404.module.css";
+
+const NotFound = ()=>{
+    return(
+        <div>
+            <h1 className={style.center}>404 error | Page Not Found</h1>
+        </div>
+    )
+}
+
+export default NotFound;
